ad714f1771a0b24b7576d6558747c3a0a9e6b910
Matylda Dygas <N00232186@iadt.ie>
2025-02-21 12:01:27 +0000

    extracting data

--- JohnsLabs/CA/sketch.js
+++ JohnsLabs/CA/sketch.js
@@ -1,12 +1,28 @@
 let data;
+let rawData = [];
 
 function preload(){
     data = loadTable('data/population.csv', 'csv', 'header')
 }
 
 function setup(){
+    cleanData();
 
 }
 function draw(){
     
+}
+
+function cleanData(){
+    //loops through each row in the data, takes out the object and puts it into the rawData array
+    for (let i=0; i<data.rows.length; i++){
+        rawData.push(data.rows[i].obj)
+    }
+
+    //loops through the rawData array, takes the variables in the year and population_number column and changes them into integers
+    for (let i=0; i<rawData.length; i++){
+        rawData[i].Year = parseInt(rawData[i].Year)
+        rawData[i].Population_number = parseInt(rawData[i].Population_number)
+    }
+
 }
