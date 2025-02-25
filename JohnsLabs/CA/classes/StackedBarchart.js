7ca3c1fed376edb727680f3b912417b021738354
matylda dygas <n00232186@iadt.ie>
2025-02-25 14:32:06 +0000

    stacked chart working

--- JohnsLabs/CA/classes/StackedBarchart.js
+++ JohnsLabs/CA/classes/StackedBarchart.js
@@ -1,9 +1,8 @@
 class StackedBarchart {
-    constructor(_data, _xValue, _yValue,_yValue2, _chartHeight, _chartWidth, _barWidth, _margin, _axisThickness, _chartPosX, _chartPosY){
+    constructor(_data, _xValue, _yValues, _chartHeight, _chartWidth, _barWidth, _margin, _axisThickness, _chartPosX, _chartPosY) {
         this.data = _data;
         this.xValue = _xValue;
-        this.yValue = _yValue;
-        this.yValue2 = _yValue2;
+        this.yValues = _yValues;
         this.chartHeight = _chartHeight;
         this.chartWidth = _chartWidth;
         this.barWidth = _barWidth;
@@ -12,96 +11,138 @@ class StackedBarchart {
         this.axisTickThickness = 1;
         this.chartPosX = _chartPosX;
         this.chartPosY = _chartPosY;
-        this.gap = (this.chartWidth - (this.data.length * this.barWidth) - (this.margin*2))/(this.data.length-1);
-        this.scaler = this.chartHeight/(max(this.data.map(row => row[this.yValue])));
+        this.gap = (this.chartWidth - (this.data.length * this.barWidth) - (this.margin * 2)) / (this.data.length - 1);
+
+        this.total = this.data.map((row) => {
+            return row['Female_25'] + row['Male_25']
+        })
+
+        this.scaler = this.chartHeight / (max(this.total));
         this.axisColour = color(255);
         this.axisTickColour = color(255);
         this.barColour = color(255);
         this.barColour2 = color(25);
-        this.axisTextColour = color(255); 
+        this.axisTextColour = color(255);
         this.numTicks = 5;
         this.tickLength = 10;
         this.barColours = [color(255), color(25)];
     }
 
-    renderBars(){
+    // renderBars(){
 
-        let yValues = [this.yValue, this.yValue2];
-        push()
-            translate(this.chartPosX, this.chartPosY)
-            push()
-            translate(this.margin, 0)
-            for(let i = 0; i<this.data.length; i++) {
-                let xPos = i*(this.barWidth + this.gap);
-                rect(0,this.barWidth, -this.data[i][this.yValue]*this.scaler);
-                push();
-                translate(xPos,0);
-                push();
-                
-                
-                //rect(xPos,0,this.barWidth, -this.data[i][this.yValue]*this.scaler);
+    //     let yValues = [this.yValue, this.yValue2];
+    //     push()
+    //         translate(this.chartPosX, this.chartPosY)
+    //         push()
+    //         translate(this.margin, 0)
+    //         for(let i = 0; i<this.data.length; i++) {
+    //             let xPos = i*(this.barWidth + this.gap);
+    //             rect(xPos,this.barWidth, -this.data[i][this.yValue]*this.scaler);
+    //             // rect(xPos,this.barWidth, -this.data[i][this.yValue2]*this.scaler);
+    //             // push();
+    //             // translate(xPos,0);
+    //             push();
 
-                for(let j = 0; j<yValues.length; j++) {
+
+    //             //rect(xPos,0,this.barWidth, -this.data[i][this.yValue]*this.scaler);
+
+    //             // for(let j = 0; j<yValues.length; j++) {
+    //             // fill(this.barColours[j]);
+    //             // rect(0,this.barWidth, -this.data[i][yValues[j]]*this.scaler);
+    //             // translate(0,-this.data[i][yValues[j]]*this.scaler-1);
+    //             // }
+    //             // pop();
+    //             pop();
+    //         }
+
+    //         pop()
+    //     pop()
+    // }
+
+    renderBars() {
+        // Function to render the bars on the chart.
+
+        push(); // Saves the current drawing.
+        translate(this.chartPosX, this.chartPosY); // Translates the origin to the chart's position.
+
+        push(); // Saves the drawing state again for a nested transformation.
+        translate(this.margin, 0); // Adds margin to the left of the chart for positioning the bars.
+
+        for (let i = 0; i < this.data.length; i++) {
+            let xPos = (this.barWidth + this.gap) * i;
+
+            push();
+            translate(xPos, 0); // shift over the x according to where we want the bar(s) drawn            
+
+            push();
+            // two values in y axis, male and female
+            // so loop twice
+            // each iteration draw a rectangle and we'll give it a different colour, otherwise won't see them
+            for (let j = 0; j < this.yValues.length; j++) {
                 fill(this.barColours[j]);
-                rect(0,this.barWidth, -this.data[i][yValues[j]]*this.scaler);
-                translate(0,-this.data[i][yValues[j]]*this.scaler-1);
-                }
-                pop();
-                pop();
-            }
-
-            pop()
-        pop()
-    }
-    renderAxis(){
-        push()
-            translate(this.chartPosX, this.chartPosY)
-            noFill();
-            stroke(this.axisColour);
-            strokeWeight(this.axisThickness)
-            line (0,0,0, -this.chartHeight)
-            line (0,0, this.chartWidth,0)
-        pop()
-    }
-    renderLabels(){
-        push()
-            translate(this.chartPosX, this.chartPosY)
-           
-            push()
-            
-            translate(this.margin, 0)
-            for(let i = 0; i<this.data.length; i++) {
-                let xPos = i*(this.barWidth + this.gap);
-              
-                fill(this.axisTextColour);
                 noStroke();
-                textAlign(LEFT,CENTER)
-                textSize(8);
 
-                push()
-                    translate(xPos + (this.barWidth/2), 10)
-                    rotate(45)
-                    text(this.data[i][this.xValue],0,0);
-                pop()
-            
+                rect(0, 0, this.barWidth, -this.data[i][this.yValues[j]] * this.scaler);
+                translate(0, -this.data[i][this.yValues[j]] * this.scaler - 1);
             }
+
+            pop();
+            pop();
+        }
+
+        pop(); // Restores the drawing state after rendering the bars.
+        pop(); // Restores the original drawing state after positioning the chart.
+    }
+
+
+    renderAxis() {
+        push()
+        translate(this.chartPosX, this.chartPosY)
+        noFill();
+        stroke(this.axisColour);
+        strokeWeight(this.axisThickness)
+        line(0, 0, 0, -this.chartHeight)
+        line(0, 0, this.chartWidth, 0)
+        pop()
+    }
+    renderLabels() {
+        push()
+        translate(this.chartPosX, this.chartPosY)
+
+        push()
+
+        translate(this.margin, 0)
+        for (let i = 0; i < this.data.length; i++) {
+            let xPos = i * (this.barWidth + this.gap);
+
+            fill(this.axisTextColour);
+            noStroke();
+            textAlign(LEFT, CENTER)
+            textSize(8);
+
+            push()
+            translate(xPos + (this.barWidth / 2), 10)
+            rotate(45)
+            text(this.data[i][this.xValue], 0, 0);
             pop()
+
+        }
+        pop()
 
         pop()
     }
-    renderTicks(){
+    renderTicks() {
         push()
-            translate(this.chartPosX, this.chartPosY)
-            noFill();
-            stroke(this.axisTickColour);
-            strokeWeight(this.axisTickThickness)
-            let tickIncrement = this.chartHeight/this.numTicks;
-            for (let i = 0; i <= this.numTicks; i++){
-                line(0,-tickIncrement*i,-this.tickLength,-tickIncrement*i);
-            }
+        translate(this.chartPosX, this.chartPosY)
+        noFill();
+        stroke(this.axisTickColour);
+        strokeWeight(this.axisTickThickness)
+        let tickIncrement = this.chartHeight / this.numTicks;
+        for (let i = 0; i <= this.numTicks; i++) {
+            line(0, -tickIncrement * i, -this.tickLength, -tickIncrement * i);
+        }
 
         pop()
     }
 }
-    
-    
+
--- JohnsLabs/CA/sketch.js
+++ JohnsLabs/CA/sketch.js
@@ -6,25 +6,25 @@ let rawData1 = [];
 let charts = [];
 let data1 = [];
 
-function preload(){
+function preload() {
     data = loadTable('data/Population.csv', 'csv', 'header')
     data1 = loadTable('data/maleFemale.csv', 'csv', 'header')
 }
 
-function setup(){
-    createCanvas(900,900);
+function setup() {
+    createCanvas(1500, 1000);
     cleanData();
     noLoop();
     //creating a BarChart object with these parameters and putting it in an array called charts
     charts.push(new BarChart(rawData, "County", "Population_number", 400, 400, 10, 15, 2, 50, 410));
 
-    charts.push(new HorizontalBarchart(rawData, "County", "Population_number", 400, 400, 10, 15, 2, 500, 450));
-    charts.push(new StackedBarchart(rawData1, "County", "Female_25","Female_25", 400, 400, 10, 15, 2, 50, 850));
-charts.push(new ClusterChart(rawData1, "County", "Female_25","Male_25", 400, 400, 10, 15, 2, 450, 850));
+    charts.push(new HorizontalBarchart(rawData, "County", "Population_number", 400, 400, 10, 15, 2, 650, 450));
+    charts.push(new StackedBarchart(rawData1,"County", ["Female_25", "Male_25"], 400, 400, 10, 15, 2, 50, 950));
+    charts.push(new ClusterChart(rawData1, "County", "Female_25", "Male_25", 400, 400, 10, 15, 2, 550, 950));
 
 }
-function draw(){
-    background(245,34,15);
+function draw() {
+    background(245, 34, 15);
     //takes each barchart in the charts array and renders it piece by piece on the canvas
     charts.forEach(chart => {
         chart.renderBars();
@@ -34,27 +34,27 @@ function draw(){
     });
 }
 
-function cleanData(){
+function cleanData() {
     //loops through each row in the data, takes out the object and puts it into the rawData array
-    for (let i=0; i<data.rows.length; i++){
+    for (let i = 0; i < data.rows.length; i++) {
         rawData.push(data.rows[i].obj)
     }
-    for (let i=0; i<data1.rows.length; i++){
+    for (let i = 0; i < data1.rows.length; i++) {
         rawData1.push(data1.rows[i].obj)
     }
 
     //loops through the rawData array, takes the variables in the year and population_number column and changes them into integers
-    for (let i=0; i<rawData.length; i++){
+    for (let i = 0; i < rawData.length; i++) {
         rawData[i].Year = parseInt(rawData[i].Year)
         rawData[i].Population_number = parseInt(rawData[i].Population_number)
     }
 
-    for (let i=0; i<rawData.length; i++){
+    for (let i = 0; i < rawData.length; i++) {
         rawData[i].Year = parseInt(rawData[i].Year)
         rawData[i].Population_number = parseInt(rawData[i].Population_number)
     }
 
-    for (let i=0; i<rawData1.length; i++){
+    for (let i = 0; i < rawData1.length; i++) {
         rawData1[i].Year = parseInt(rawData1[i].Year)
         rawData1[i].Population_number = parseInt(rawData1[i].Population_number)
         rawData1[i].Female_25 = parseInt(rawData1[i].Female_25)
